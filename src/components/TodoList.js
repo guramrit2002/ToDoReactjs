2c0802051636d8137c1f80979826d57643fa3ecd
Amrit5s2002 <guramrit1066@gmail.com>
2024-01-15 21:37:28 +0530

    buttons

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,16 +4,20 @@ import React from 'react';
 function TodoList({ activeButton, alltodo, completedTodos, handelDeleteTodo, handelCompleteTodo }) {
   return (
     <div className='todo-list-area'>
+
+
       {activeButton === 'todo' && alltodo.map((item, index) => {
         return (
           <div className='todo-list-item' key={index}>
             <h3>{item.newtitle}</h3>
             <p>{item.newdescription}</p>
             <button type='button' onClick={() => { handelDeleteTodo(index) }} className='deletebutton'>Delete</button>
-            <button type='button' onClick={() => { handelCompleteTodo(index) }} className='deletebutton'>Done</button>
+            <button type='button' onClick={() => { handelCompleteTodo(index) }} className='donebutton'>Done</button>
           </div>
         )
       })}
+
+
       {activeButton === 'done' && completedTodos.map((item, index) => {
         console.log(completedTodos)
         return (
